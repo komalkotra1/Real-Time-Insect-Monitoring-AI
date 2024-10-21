facddfc553a20cfb65cb70471c52b0cc654e43af
Komal Kotra <komalkotra1@gmail.com>
2024-10-21 23:10:56 +0200

    adding folders with output files

--- Tracking1/tracking 2.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('C:/Users/komal/University/Machine_learning/yolov8/runs/detect/train18/weights/best.pt')
-
-results = model.track(source='C:/Users/komal/University/Machine_learning/data/Videos/MAH00088.MP4', conf=0.3, iou=0,show=True, tracker="bytetrack.yaml")
